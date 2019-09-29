674b68e18e3bcfc3b369b25d0480db282f256a10
o-Cytokine-o <b7072@gmail.com>
2019-09-30 01:10:40 +0900

    09/30webカメラのテスト

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-import janken_AI
+import polls.janken_AI
 
 # Create your views here.
 def index(request):
